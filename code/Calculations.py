15b26748cd969bca0fe02626f5a5706761637549
FHuppertz <huppertzfabian@gmail.com>
2025-10-20 14:50:20 +0200

    All team data and updated report

--- code/Calculations.py
+++ code/Calculations.py
@@ -4,73 +4,91 @@ import matplotlib.pyplot as plt
 
 from Functions import End_Pose, Cov
 
+manual_file_path1 = '../data/group1/manual_measurements.csv'
+encoder_file_path1 = '../data/group1/robot_path.csv'
 
-encoder_file_path = './data/robot_path.csv'
-manual_file_path = './data/manual_measurements.csv'
-manual_file_path2 = './data/rahul_measurements.csv'
-manual_file_path3 = './data/bhavesh_measurements.csv'
+manual_file_path2 = '../data/group2/calculated_translated_readings.csv'
+encoder_file_path2 = '../data/group2/robot_path.csv'
 
-try:
-    encoder_df = pd.read_csv(encoder_file_path, sep=r'\s+', header=None)
+manual_file_path3 = '../data/group3/manual_measurements_new.csv'
+encoder_file_path3 = '../data/group3/combined_trajectories.csv'
 
-    manual_df = pd.read_csv(manual_file_path, header=None)
-    manual_df2 = pd.read_csv(manual_file_path2, header=None)
-    manual_df3 = pd.read_csv(manual_file_path3)
-except FileNotFoundError:
-    print(f"Error: File was not found. Please double-check the path.")
-    # Exiting the function if the file cannot be loaded
-    exit()
+manual_file_path4 = '../data/group4/Robot_End_Poses_Manual.csv'
+encoder_file_path4 = '../data/group4/robot_poses.csv'
 
 
-# Rename columns for clarity (matching the data structure: X, Y, Angle)
-encoder_df.columns = ['Y', 'X', 'Angle']
-encoder_df['X'] *= -1.0
-encoder_df['Y'] *= -1.0
-encoder_df['Angle'] *= -1.0
-encoder_df['Angle'] += np.pi/2
+manual_df1 = pd.read_csv(manual_file_path1, header=None)
+encoder_df1 = pd.read_csv(encoder_file_path1, sep=r'\s+', header=None)
 
-#print(encoder_df['Angle'])
+manual_df2 = pd.read_csv(manual_file_path2, header=None)
+encoder_df2 = pd.read_csv(encoder_file_path2, sep=r'\s+', header=None)
 
-manual_df.columns = ['Lx', 'Ly', 'Rx', 'Ry']
-manual_df[['X', 'Y', 'Angle']] = manual_df.apply(End_Pose, axis=1, result_type='expand')
+manual_df3 = pd.read_csv(manual_file_path3, header=None)
+encoder_df3 = pd.read_csv(encoder_file_path3, header=None)
 
-manual_df2.columns = ['Lx', 'Ly', 'Rx', 'Ry']
-manual_df2['Lx'] -= 6.9
-manual_df2['Ly'] += 4.75
-manual_df2['Rx'] -= 6.9
-manual_df2['Ry'] += 4.75
-manual_df2[['X', 'Y', 'Angle']] = manual_df2.apply(End_Pose, axis=1, result_type='expand')
+manual_df4 = pd.read_csv(manual_file_path4, header=None)
+encoder_df4 = pd.read_csv(encoder_file_path4, sep=r'\s+', header=None)
 
+# Rename columns for clarity (matching the data structure: X, Y, Theta)
+encoder_df1.columns = ['Y', 'X', 'Theta']
+encoder_df1['X'] *= -1.0
+encoder_df1['Y'] *= -1.0
+encoder_df1['Theta'] *= -1.0
+encoder_df1['Theta'] += np.pi/2
+manual_df1.columns = ['Lx', 'Ly', 'Rx', 'Ry']
+manual_df1[['X', 'Y', 'Theta']] = manual_df1.apply(End_Pose, axis=1, result_type='expand')
+manual_df1.to_csv('../data/group1/manual_end_poses_calc.csv', index=False)
+
+
+manual_df2.columns = ['X', 'Y', 'Theta']
+manual_df2['Theta'] *= np.pi/180
+encoder_df2.columns = ['Y', 'X', 'Theta']
+encoder_df2['X'] *= -1.0
+encoder_df2['Y'] *= -1.0
+encoder_df2['Theta'] *= -1.0
+encoder_df2['Theta'] += np.pi/2
+
+manual_df3.columns = ['X', 'Y', 'Theta']
 manual_df3['Theta'] *= np.pi/180
+encoder_df3.columns = ['Y', 'X', 'Theta']
+encoder_df3['X'] *= -1.0
+encoder_df3['Y'] *= -1.0
+encoder_df3['Theta'] *= -1.0
+encoder_df3['Theta'] += np.pi/2
+
+manual_df4.columns = ['Y', 'X', 'Theta']
+manual_df4['Theta'] *= np.pi/180
+encoder_df4.columns = ['Y', 'X', 'Theta']
+encoder_df4['Theta'] -= np.pi/2
 
 # Plot the results
 plt.figure()
 
-angles = [
-    encoder_df['Angle'].min(),
-    encoder_df['Angle'].max(),
-    manual_df['Angle'].min(),
-    manual_df['Angle'].max(),
-    manual_df2['Angle'].min(),
-    manual_df2['Angle'].max(),
-    manual_df3['Theta'].min(),
-    manual_df3['Theta'].max(),
+Thetas = [
+    encoder_df1['Theta'].min(),
+    encoder_df1['Theta'].max(),
+    manual_df1['Theta'].min(),
+    manual_df1['Theta'].max(),
+    encoder_df2['Theta'].min(),
+    encoder_df2['Theta'].max(),
+    manual_df2['Theta'].min(),
+    manual_df2['Theta'].max(),
 ]
 
-max_angle = max(angles)
-min_angle = min(angles)
+max_Theta = max(Thetas)
+min_Theta = min(Thetas)
 
 manual_scatter = plt.scatter(
-    manual_df['Y'],
-    manual_df['X'],
+    manual_df1['Y'],
+    manual_df1['X'],
     label='End Poses Team 1',
     marker='x',
     s=20,
     alpha=0.5,
-    c=manual_df['Angle'],
+    c=manual_df1['Theta'],
     cmap='plasma',
-    vmin=float(min_angle),
-    vmax=float(max_angle)
+    vmin=float(min_Theta),
+    vmax=float(max_Theta)
 )
 
 manual_scatter2 = plt.scatter(
@@ -80,79 +98,81 @@ manual_scatter2 = plt.scatter(
     marker='s',
     s=20,
     alpha=0.5,
-    c=manual_df2['Angle'],
+    c=manual_df2['Theta'],
     cmap='plasma',
-    vmin=float(min_angle),
-    vmax=float(max_angle)
+    vmin=float(min_Theta),
+    vmax=float(max_Theta)
 )
 
 manual_scatter3 = plt.scatter(
     manual_df3['Y'],
     manual_df3['X'],
     label='End Poses Team 3',
-    marker='^',
+    marker='o',
     s=20,
     alpha=0.5,
     c=manual_df3['Theta'],
     cmap='plasma',
-    vmin=float(min_angle),
-    vmax=float(max_angle)
+    vmin=float(min_Theta),
+    vmax=float(max_Theta)
 )
 
-manual_scatter3 = plt.scatter(
-    manual_df3['Y'],
-    manual_df3['X'],
+manual_scatter4 = plt.scatter(
+    manual_df4['Y'],
+    manual_df4['X'],
     label='End Poses Team 4',
     marker='^',
     s=20,
     alpha=0.5,
-    c=manual_df3['Theta'],
+    c=manual_df4['Theta'],
     cmap='plasma',
-    vmin=float(min_angle),
-    vmax=float(max_angle)
+    vmin=float(min_Theta),
+    vmax=float(max_Theta)
 )
 
-plt.title('Manual measured robot end pose')
+
+plt.title('Pen measured robot end pose')
 plt.xlabel('Y (cm)')
 plt.ylabel('X (cm)')
 plt.axis('equal')
 
 cbar = plt.colorbar(manual_scatter)
-cbar.set_label('Angle (Radians)', rotation=270, labelpad=15)
-cbar.set_ticks(list(np.linspace(min_angle, max_angle, 15)))
+cbar.set_label('Theta (Radians)', rotation=270, labelpad=15)
+cbar.set_ticks(list(np.linspace(min_Theta, max_Theta, 15)))
 
 plt.grid(True, linestyle='--', alpha=0.6)
 plt.legend(loc='lower right')
-plt.savefig('figures/all_pose_scatter_plot.png', dpi=500)
+plt.savefig('../figures/all_pose_scatter_plot.png', dpi=500)
 plt.close()
 
 
+# TEAM 1
 plt.figure()
 
 manual_scatter = plt.scatter(
-    manual_df['Y'],
-    manual_df['X'],
+    manual_df1['Y'],
+    manual_df1['X'],
     label='Manual End Poses',
     marker='x',
     s=20,
     alpha=0.5,
-    c=manual_df['Angle'],
+    c=manual_df1['Theta'],
     cmap='plasma',
-    vmin=float(min_angle),
-    vmax=float(max_angle)
+    vmin=float(min_Theta),
+    vmax=float(max_Theta)
 )
 
 encoder_scatter = plt.scatter(
-    encoder_df['Y'],
-    encoder_df['X'],
+    encoder_df1['Y'],
+    encoder_df1['X'],
     label='Encoder Data',
     #marker='.',
     s=0.1,
     alpha=0.5,
-    c=encoder_df['Angle'],
+    c=encoder_df1['Theta'],
     cmap='plasma',
-    vmin=float(min_angle),
-    vmax=float(max_angle)
+    vmin=float(min_Theta),
+    vmax=float(max_Theta)
 )
 
 
@@ -162,13 +182,148 @@ plt.ylabel('X (cm)')
 plt.axis('equal')
 
 cbar = plt.colorbar(manual_scatter)
-cbar.set_label('Angle (Radians)', rotation=270, labelpad=15)
-cbar.set_ticks(list(np.linspace(min_angle, max_angle, 15)))
+cbar.set_label('Theta (Radians)', rotation=270, labelpad=15)
+cbar.set_ticks(list(np.linspace(min_Theta, max_Theta, 15)))
 
 plt.grid(True, linestyle='--', alpha=0.6)
 plt.legend(loc='lower right')
-plt.savefig('figures/team1_pose_scatter_plot.png', dpi=500)
+plt.savefig('../figures/team1_pose_scatter_plot.png', dpi=500)
 plt.close()
 
+
+
+# TEAM 2
+plt.figure()
+
+manual_scatter = plt.scatter(
+    manual_df2['Y'],
+    manual_df2['X'],
+    label='Manual End Poses',
+    marker='s',
+    s=20,
+    alpha=0.5,
+    c=manual_df2['Theta'],
+    cmap='plasma',
+    vmin=float(min_Theta),
+    vmax=float(max_Theta)
+)
+
+encoder_scatter = plt.scatter(
+    encoder_df2['Y'],
+    encoder_df2['X'],
+    label='Encoder Data',
+    #marker='.',
+    s=0.1,
+    alpha=0.5,
+    c=encoder_df2['Theta'],
+    cmap='plasma',
+    vmin=float(min_Theta),
+    vmax=float(max_Theta)
+)
+
+
+plt.title('Robot end pose and path (Team 2)')
+plt.xlabel('Y (cm)')
+plt.ylabel('X (cm)')
+plt.axis('equal')
+
+cbar = plt.colorbar(manual_scatter)
+cbar.set_label('Theta (Radians)', rotation=270, labelpad=15)
+cbar.set_ticks(list(np.linspace(min_Theta, max_Theta, 15)))
+
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.legend(loc='lower right')
+plt.savefig('../figures/team2_pose_scatter_plot.png', dpi=500)
+plt.close()
+
+
+# TEAM 3
+plt.figure()
+
+manual_scatter = plt.scatter(
+    manual_df3['Y'],
+    manual_df3['X'],
+    label='Manual End Poses',
+    marker='o',
+    s=20,
+    alpha=0.5,
+    c=manual_df3['Theta'],
+    cmap='plasma',
+    vmin=float(min_Theta),
+    vmax=float(max_Theta)
+)
+
+encoder_scatter = plt.scatter(
+    encoder_df3['Y'],
+    encoder_df3['X'],
+    label='Encoder Data',
+    #marker='.',
+    s=0.1,
+    alpha=0.5,
+    c=encoder_df3['Theta'],
+    cmap='plasma',
+    vmin=float(min_Theta),
+    vmax=float(max_Theta)
+)
+
+
+plt.title('Robot end pose and path (Team 3)')
+plt.xlabel('Y (cm)')
+plt.ylabel('X (cm)')
+plt.axis('equal')
+
+cbar = plt.colorbar(manual_scatter)
+cbar.set_label('Theta (Radians)', rotation=270, labelpad=15)
+cbar.set_ticks(list(np.linspace(min_Theta, max_Theta, 15)))
+
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.legend(loc='lower right')
+plt.savefig('../figures/team3_pose_scatter_plot.png', dpi=500)
+plt.close()
+
+
+# TEAM 4
+plt.figure()
+
+manual_scatter = plt.scatter(
+    manual_df4['Y'],
+    manual_df4['X'],
+    label='Manual End Poses',
+    marker='^',
+    s=20,
+    alpha=0.5,
+    c=manual_df4['Theta'],
+    cmap='plasma',
+    vmin=float(min_Theta),
+    vmax=float(max_Theta)
+)
+
+encoder_scatter = plt.scatter(
+    encoder_df4['Y'],
+    encoder_df4['X'],
+    label='Encoder Data',
+    #marker='.',
+    s=0.1,
+    alpha=0.5,
+    c=encoder_df4['Theta'],
+    cmap='plasma',
+    vmin=float(min_Theta),
+    vmax=float(max_Theta)
+)
+
+
+plt.title('Robot end pose and path (Team 4)')
+plt.xlabel('Y (cm)')
+plt.ylabel('X (cm)')
+plt.axis('equal')
+
+cbar = plt.colorbar(manual_scatter)
+cbar.set_label('Theta (Radians)', rotation=270, labelpad=15)
+cbar.set_ticks(list(np.linspace(min_Theta, max_Theta, 15)))
+
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.legend(loc='lower right')
+plt.savefig('../figures/team4_pose_scatter_plot.png', dpi=500)
+plt.close()
 
 print('Finished plotting')
