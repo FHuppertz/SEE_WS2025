daa782ed650e29070822b052aa6b3bd0a6adedd0
Hari <harikrishnam0711@gmail.com>
2025-11-06 17:06:45 +0100

    assignment04

---
+++ assignment04/cam_calib.py
@@ -0,0 +1,62 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*8,3), np.float32)
+objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+images = glob.glob('*.jpg')
+
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (8,6), None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (8,6), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
+
+# cv.destroyAllWindows()
+
+
+
+# Calibrate the camera
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+print("Calibration successful:", ret)
+print("\nCamera matrix:\n", mtx)
+print("\nDistortion coefficients:\n", dist)
+
+# Save calibration results
+np.savez("camera_calibration_9x7.npz", mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
+
+# Optional: test undistortion on one image
+img = cv.imread(images[0])
+h, w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+
+# Undistort
+dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+
+cv.imshow('Original', img)
+cv.imshow('Undistorted', dst)
+cv.waitKey(0)
+cv.destroyAllWindows()
